ebd5a695fe07efd877a2e8cb0c8b8ff8e7a1ac70
Derick Didas <64195134+DerickDidas@users.noreply.github.com>
2024-07-31 13:50:48 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'I Have Something for  U'.split('')
+const text = 'Have Something for U'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
