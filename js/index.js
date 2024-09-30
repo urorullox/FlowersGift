fa4921d32c148cac750ed608c0783573107cc61c
urorullox <orullox.08.x@gmail.com>
2024-09-30 20:24:53 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'Aku Punya Sesuatu Buat Ceweknya Jess yang sangat lutsyuu (ditap dulu yaa layarnyaa sebelum open)'.split('')
+const text = 'Aku Punya Sesuatu Buat Ceweknya Jess yang sangat lutsyuu,dengerin duluu yaa lagunya sebelum open (tap aja layarnya dimanapun)'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
