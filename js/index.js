71b68f5230487d15f8c507b5a9880feb12b7f6a8
urorullox <orullox.08.x@gmail.com>
2024-09-30 08:43:53 +0800

    index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'Aku Punya Sesuatu Buat Ceweknya Jess 🤭'.split('')
+const text = 'Aku Punya Sesuatu Buat Ceweknya Jess hehe'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
