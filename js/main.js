eab1714f2a6290dcd4a6f6b7cbff37630a749d24
urorullox <orullox.08.x@gmail.com>
2024-09-30 08:41:09 +0800

    main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('Happy Girlfriends Day 💓').split('')
+    const titles = ('Happy 1 October Diannnn 💓').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
