a05538da970b10a56669c6fcddb0d9c633d9a8bf
urorullox <orullox.08.x@gmail.com>
2024-09-30 20:09:58 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'Aku Punya Sesuatu Buat Ceweknya Jess yang sangat lutsyuu'.split('')
+const text = 'Aku Punya Sesuatu Buat Ceweknya Jess yang sangat lutsyuu (ditap dulu yaa layarnyaa sebelum open)'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
